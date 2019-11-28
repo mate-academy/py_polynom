120a2a460a1fe02d1e1bf4e5d4a2a3b51ef7f62d
Dima <dima.sapsay@gmail.com>
2019-11-28 17:55:56 +0200

    first commit

--- polynom.py
+++ polynom.py
@@ -1,6 +1,16 @@
+"""
+Polynomial of a given degree
+"""
+
+
 from typing import Callable
 
 
 def get_poly(*koeffs: float) -> Callable[[float], float]:
-
-    return lambda x: 42
+    """Calculate polinomial"""
+    def pol_sum(value):
+        lst_polinom = []
+        for indx, koeff in enumerate(koeffs):
+            lst_polinom.append(koeff * value ** indx)
+        return sum(lst_polinom)
+    return pol_sum
