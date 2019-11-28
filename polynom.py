e1837c5cd13f7bcb21461f6822fcaffac33b431b
Senku1 <morhaliuk@gmail.com>
2019-11-28 13:20:21 +0200

    Solved

--- polynom.py
+++ polynom.py
@@ -1,6 +1,19 @@
+"""docstring"""
 from typing import Callable
 
 
 def get_poly(*koeffs: float) -> Callable[[float], float]:
+    """
 
-    return lambda x: 42
+    :param koeffs: float
+    :return:
+    """
+    def poly_sum(arg):
+        polinom = []
+        max_pow = len(koeffs) - 1
+        for idx, coeff in enumerate(koeffs):
+            power = max_pow - idx
+            polinom.append(coeff * arg ** power)
+        return sum(polinom)
+
+    return poly_sum
