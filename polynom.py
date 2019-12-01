fb8262596e434316434055e96dad65e073a83c0f
YuPo13 <yuliya.povzlo@gmail.com>
2019-12-02 00:56:38 +0200

    Callable is easy after being learnt.

--- polynom.py
+++ polynom.py
@@ -1,6 +1,24 @@
+"""The script takes coefficients and
+value of variable and calculates polynomial value"""
 from typing import Callable
 
 
 def get_poly(*koeffs: float) -> Callable[[float], float]:
+    """
+    Convert the list of coefficients provided into the one
+    used for Horner's polynomial evaluation algorithm calculation
+    :param koeffs: list of coefficients to be multiplied
+    by respective parts of polynom
+    :return: polynomial value calculated in currying function
+    """
+    coefficients = koeffs[::-1]
 
-    return lambda x: 42
+    def inner_f(value):
+        """Currying function that takes the value of polynomial variable
+        and calculates polynomial value
+        based on coefficients processed in outer function"""
+        result = 0
+        for seq, coef in enumerate(coefficients):
+            result += coef * (value ** seq)
+        return result
+    return inner_f
